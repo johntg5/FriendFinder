8399b055bb90f8f9ed78e65b1cf5137ba7191bdd
Tyler Johnson <johntg5@gmail.com>
2017-12-10 17:15:51 +0000

    second commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,35 @@
-
 //create array of default friends
 
 var friends = [{
-  name: "John Torpey",
-  photo: "https://static.pexels.com/photos/343717/pexels-photo-343717.jpeg",
+  name: "Charles Barkley",
+  photo: "https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzc1NjA2Nzk4/charles-barkley-9542575-1-402.jpg",
   scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
 }, {
-  name: "Laura Toner",
-  photo: "https: //static.pexels.com/photos/206481/pexels-photo-206481.jpeg",
+  name: "Samuel L. Jackson",
+  photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_UX214_CR0,0,214,317_AL_.jpg",
   scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
 }, {
-  name: "Bob Merad",
-  photo: "https://static.pexels.com/photos/249760/pexels-photo-249760.jpeg",
+  name: "Conan O'Brien",
+  photo: "http://cdn-img.instyle.com/sites/default/files/styles/684xflex/public/1431959206/051815-conan-obriens-hair-lead.jpg?itok=prYI4pOE",
   scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
-},{
-  name: "Amy Crosby",
-  photo: "https://static.pexels.com/photos/270308/pexels-photo-270308.jpeg",
+}, {
+  name: "Arnold Schwarzenegger",
+  photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI3MDc4NzUyMV5BMl5BanBnXkFtZTcwMTQyMTc5MQ@@._V1_UY1200_CR162,0,630,1200_AL_.jpg",
   scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
 }, {
-  name: "Chris Maclin",
-  photo: "https://static.pexels.com/photos/249757/pexels-photo-249757.jpeg",
+  name: "Tina Fey",
+  photo: "http://media.graytvinc.com/images/810*455/tina-fey.jpg",
   scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
 }, {
-  name: "Anna Schultz",
-  photo: "https://static.pexels.com/photos/388517/pexels-photo-388517.jpeg",
+  name: "Adam Sandler",
+  photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjQyNzM2MjM1Ml5BMl5BanBnXkFtZTcwMDE5NjI3Mg@@._V1_UY1200_CR117,0,630,1200_AL_.jpg",
   scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
 }, {
-  name: "Jack Waller",
-  photo: "https://static.pexels.com/photos/64385/pexels-photo-64385.jpeg",
+  name: "No One. Sorry You Will Die Alone",
+  photo: "https://cdn-images-1.medium.com/max/1600/0*9BYGa20RkF4rTpJU.png",
   scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
 }];
 
- //export array
+//export array
 
 module.exports = friends;
